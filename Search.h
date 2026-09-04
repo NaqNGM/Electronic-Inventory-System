a4d8701dc2668226b288e9f16bee8b2e84fd6c75
qiuqiu07 <naqiuddinhadif2@gmail.com>
2026-09-04 23:32:57 +0800

    Update Search.h

--- Search.h
+++ Search.h
@@ -1,3 +1,4 @@
+//Search.h
 #ifndef SEARCH_H
 #define SEARCH_H
 
@@ -8,5 +9,11 @@ int binarySearchById(InventoryItem arr[], int count, const std::string& targetId
 
 // UI function
 void searchItem(InventoryItem arr[], int count);
+
+int ProbabilitySearchByModel(InventoryItem array[], int count, const string& TargetModel);
+
+void ProbabilityItem(InventoryItem array[], int count);
+
+void SearchingChoices(InventoryItem arr[], int count);
 
 #endif
