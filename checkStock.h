e993f6b64cbbc64199626fe97920956e586128fe
qiuqiu07 <naqiuddinhadif2@gmail.com>
2026-09-04 23:33:34 +0800

    Create checkStock.h

---
+++ checkStock.h
@@ -0,0 +1,14 @@
+//checkStock.h
+//made by Anis Athirah
+
+#include <iostream>
+#ifndef STOCK_H
+#define STOCK_K
+#include "InventoryItem.h"
+
+using namespace std;
+
+//declared function
+void extraFunction();
+
+#endif
