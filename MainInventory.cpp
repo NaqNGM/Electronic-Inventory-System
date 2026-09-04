c365ec9d959cc5ab9a891fa32024ec64733bdf49
qiuqiu07 <naqiuddinhadif2@gmail.com>
2026-09-04 23:34:15 +0800

    Update MainInventory.cpp

--- MainInventory.cpp
+++ MainInventory.cpp
@@ -1,4 +1,5 @@
 //Main.cpp
+// BY NUR AQILAH SYAFIQAH
 
 #include <iostream>
 #include <string>
@@ -7,9 +8,9 @@
 #include <fstream> // file txt
 #include "sort.h"
 #include "search.h"
+#include "checkStock.h"
 using namespace std;
 
-// part qilah ni nnti kena ada cpp dia sendiri. later change
 InventoryItem inventoryList[100];
 int itemCount = 0;
 
@@ -23,59 +24,178 @@ string toUpper(string str) {
     }
     return str;
 }
-// BY NUR AQILAH SYAFIQAH
+
 // berfungsi utk menambah item baru
 void add() {
     string id, model, category;
     float price;
-    int qty;
+    int qty, MainMenu;
 
-
-    cout << "-----------------------------------\n";
-    cout << "            ADD NEW ITEM           \n";
-    cout << "-----------------------------------\n";
-    cout << "-----------------------------------\n";
-    cout << "            ID TEMPLATE            \n";
-    cout << "   Laptop: L**** || Phone: P****   \n";
-    cout << "   TV: T****     || Scooter: S**** \n";
-    cout << "           Cooker: C****           \n\n";
-    cout << "Enter Item ID: ";
-    cin >> id;
-    cout << "Enter Model Item: ";
-    cin.ignore();
-    getline(cin, model);
-    cout << "Enter Category : ";
-    getline(cin, category);
-    cout << "Enter Price (RM): ";
-    cin >> price;
     do
     {
-        cout << "Enter Quantity: ";
-        cin >> qty;
-        if (qty <= 0)
+        //paparan menu untuk add new item
+        cout << "-----------------------------------\n";
+        cout << "            ADD NEW ITEM           \n";
+        cout << "-----------------------------------\n";
+        cout << "-----------------------------------\n";
+        cout << "            ID TEMPLATE            \n";
+        cout << "   Laptop: L**** || Phone: P****   \n";
+        cout << "   TV: T****     || Scooter: S**** \n";
+        cout << "           Cooker: C****           \n\n";
+        cout << "1. Back to Main Menu" << endl;
+        cout << "2. Proceed" << endl;
+
+        cout << "Enter your number: ";
+        cin >> MainMenu;
+
+        if (cin.fail()) // pastikan pilihan menu nombor yang betul
         {
-            cout << "\nTHE QUANTITY IS INVALID\n\n";
+            cin.clear(); // clear error
+            cin.ignore(1000, '\n'); // clear invalid input
+            cout << "\nInvalid input! Please enter a number.\n\n";
+            system("pause");
+            system("cls");
+            continue;
         }
-    } while (qty <= 0);
-    cout << "-----------------------------------\n";
-    inventoryList[itemCount] = InventoryItem(id, model, category, price, qty);
-    itemCount++;
-    cout << "Success! Item added successfully .\n";
-    cout << "-----------------------------------\n";
 
-    system("pause");
-    system("cls");
+        if (MainMenu == 2)
+        {
+            // 1. Validation untuk ID (1 Huruf dan 4 Digit)
+            bool invalidId;
+            do {
+                invalidId = false;
+                cout << "Enter Item ID: ";
+                cin >> id;
+                id = toUpper(id);
 
-    ofstream outFile("inputfile.txt", ios::app);
-    if (outFile.is_open())
-    {
-        outFile << id << " " << model << " " << category << " "
-            << fixed << setprecision(2) << price << " " << qty << "\n";
-        outFile.close();
-    }
+                //Validate id kene ada 5 char : 1 huruf + 4 nombor
+                if (id.length() != 5 || id[0] < 'A' || id[0] > 'Z') {
+                    invalidId = true;
+                }
+                else {
+                    for (int i = 1; i < 5; i++) {
+                        if (id[i] < '0' || id[i] > '9') {
+                            invalidId = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (invalidId) {
+                    cout << "\nError: Invalid format! Must be 1 Letter and 4 Numbers (e.g., P1001).\n\n";
+                    continue; // Skip the duplicate check and restart the loop
+                }
+
+                // 2. Loop through existing inventory to see if ID matches
+                for (int i = 0; i < itemCount; i++) {
+                    if (inventoryList[i].itemId == id) {
+                        invalidId = true;
+                        cout << "\nError: Item ID '" << id << "' already exists!\n";
+                        cout << "Please enter a unique ID.\n\n";
+                        break; // Break the for-loop, repeat the do-while loop
+                    }
+                }
+            } while (invalidId);
+
+            // 2. Validation untuk model item 
+            cout << "Enter Model Item: ";
+            cin.ignore();
+            getline(cin, model);
+
+            // ganti ' ' dengan '-'
+            for (int i = 0; i < model.length(); i++) {
+                if (model[i] == ' ') {
+                    model[i] = '-';
+                }
+            }
+            //uppercase input
+            model = toUpper(model);
+
+            // 3. Validation untuk Category Laptop, Phone, TV, Scooter atau Cooker sahaja
+            bool validCategory;
+            do {
+                cout << "Enter Category (PHONE, LAPTOP, TV, SCOOTER, or COOKER): ";
+                getline(cin, category);
+                category = toUpper(category);
+
+                if (category == "PHONE" || category == "LAPTOP" ||
+                    category == "TV" || category == "SCOOTER" ||
+                    category == "COOKER") {
+                    validCategory = true;
+                }
+                else {
+                    validCategory = false;
+                    cout << "\nError: Invalid category! Please enter one of the specified options.\n\n";
+                }
+            } while (!validCategory);
+
+            //4. Validation untuk Price
+            bool validPrice;
+            do {
+                validPrice = true;
+                cout << "Enter Price (RM): ";
+                cin >> price;
+
+                //untuk pastikan price ialah nombor dan lebih dari 0
+                if (cin.fail() || price <= 0) {
+                    cin.clear();
+                    cin.ignore(10000, '\n');
+                    validPrice = false;
+                    cout << ">> Error: Price must be a valid number only and greater than 0.\n\n";
+                }
+            } while (!validPrice);
+
+            // 5. Validation untuk Quantity 
+            bool validQty;
+            do {
+                validQty = true;
+                cout << "Enter Quantity: ";
+                cin >> qty;
+
+                //untuk pastikan kuantiti ialah nombor dan lebih dari 0
+                if (cin.fail() || qty <= 0) {
+                    cin.clear();
+                    cin.ignore(10000, '\n');
+                    validQty = false;
+                    cout << ">> Error: Quantity must be a valid number only and greater than 0.\n\n";
+                }
+            } while (!validQty);
+
+            //simpan ke dalam array
+            cout << "-----------------------------------\n";
+            inventoryList[itemCount] = InventoryItem(id, model, category, price, qty);
+            itemCount++;
+            cout << "Success! Item added successfully .\n";
+            cout << "-----------------------------------\n";
+
+            system("pause");
+            system("cls");
+
+            ofstream outFile("inputfile.txt", ios::app);
+            if (outFile.is_open())
+            {
+                outFile << id << " " << model << " " << category << " "
+                    << fixed << setprecision(2) << price << " " << qty << "\n";
+                outFile.close();
+            }
+        }
+        else if (MainMenu == 1)
+        {
+            system("cls");
+            return;
+        }
+        else
+        {
+            cout << "\nInvalid Number!!! Try again.\n\n" << endl;
+            system("pause");
+            system("cls");
+        }
+
+    } while (true);
+
 }
 
-//function untuk divices 
+//function untuk display device
 void displayPhone() {
     cout << "\n----------------------------\n";
     cout << "            PHONE             \n";
@@ -94,6 +214,7 @@ void displayPhone() {
     }
     cout << "-----------------------------------\n";
     system("pause");
+    system("cls");
 }
 
 void displayLaptop() {
@@ -102,7 +223,7 @@ void displayLaptop() {
     cout << "------------------------------\n";
     bool found = false;
     for (int i = 0; i < itemCount; i++) {
-        if (toUpper(inventoryList[i].category) == "PHONE") {
+        if (toUpper(inventoryList[i].category) == "LAPTOP") {
             cout << " Model : " << inventoryList[i].ModelItem
                 << " | Price : RM " << fixed << setprecision(2) << inventoryList[i].price
                 << " | Quantity : " << inventoryList[i].quantity << endl;
@@ -114,6 +235,7 @@ void displayLaptop() {
     }
     cout << "-----------------------------------\n";
     system("pause");
+    system("cls");
 }
 
 
@@ -123,7 +245,7 @@ void displayTV() {
     cout << "-----------------------------------\n";
     bool found = false;
     for (int i = 0; i < itemCount; i++) {
-        if (toUpper(inventoryList[i].category) == "PHONE") {
+        if (toUpper(inventoryList[i].category) == "TV") {
             cout << " Model : " << inventoryList[i].ModelItem
                 << " | Price : RM " << fixed << setprecision(2) << inventoryList[i].price
                 << " | Quantity : " << inventoryList[i].quantity << endl;
@@ -135,6 +257,7 @@ void displayTV() {
     }
     cout << "-----------------------------------\n";
     system("pause");
+    system("cls");
 }
 
 void displayScooter() {
@@ -143,7 +266,7 @@ void displayScooter() {
     cout << "-----------------------------------\n";
     bool found = false;
     for (int i = 0; i < itemCount; i++) {
-        if (toUpper(inventoryList[i].category) == "PHONE") {
+        if (toUpper(inventoryList[i].category) == "SCOOTER") {
             cout << " Model : " << inventoryList[i].ModelItem
                 << " | Price : RM " << fixed << setprecision(2) << inventoryList[i].price
                 << " | Quantity : " << inventoryList[i].quantity << endl;
@@ -155,6 +278,7 @@ void displayScooter() {
     }
     cout << "-----------------------------------\n";
     system("pause");
+    system("cls");
 }
 
 void displayCooker() {
@@ -163,7 +287,7 @@ void displayCooker() {
     cout << "-----------------------------------\n";
     bool found = false;
     for (int i = 0; i < itemCount; i++) {
-        if (toUpper(inventoryList[i].category) == "PHONE") {
+        if (toUpper(inventoryList[i].category) == "COOKER") {
             cout << " Model : " << inventoryList[i].ModelItem
                 << " | Price : RM " << fixed << setprecision(2) << inventoryList[i].price
                 << " | Quantity : " << inventoryList[i].quantity << endl;
@@ -175,11 +299,13 @@ void displayCooker() {
     }
     cout << "-----------------------------------\n";
     system("pause");
+    system("cls");
 }
 
 //category untuk switch case 
 void CategoryMenu() {
     int categoryChoice;
+
 
     cout << "\n----------------------------------\n";
     cout << "         SELECT CATEGORY            \n";
@@ -216,11 +342,7 @@ void CategoryMenu() {
     }
 }
 
-void extraFunction() {
-    cout << "\nEXTRA\n";
-}
 
-// BY AQILAH 
 int main() { // SUPPOSEDLY THIS SHOULD BE MAIN.CPP START FROM HERE
     ifstream inFile("inputfile.txt");
 
@@ -251,7 +373,7 @@ int main() { // SUPPOSEDLY THIS SHOULD BE MAIN.CPP START FROM HERE
         cout << "2. Category Menu\n";
         cout << "3. Sort Items\n";
         cout << "4. Search Items\n";
-        cout << "5. Extra Function\n";
+        cout << "5. Check Stock\n";
         cout << "6. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
@@ -268,7 +390,7 @@ int main() { // SUPPOSEDLY THIS SHOULD BE MAIN.CPP START FROM HERE
             sortItem(inventoryList, itemCount);
             break;
         case 4:
-            SearchingChoices(inventoryList, inventoryList, itemCount);
+            SearchingChoices(inventoryList, itemCount);
             break;
         case 5:
             extraFunction();
@@ -278,6 +400,8 @@ int main() { // SUPPOSEDLY THIS SHOULD BE MAIN.CPP START FROM HERE
             break;
         default:
             cout << "Invalid choice! Please try again.\n";
+            system("pause");
+            system("cls");
         }
     } while (choice != 6);
 
